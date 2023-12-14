4b445422d58bd4964b26125f23013d8ac022e16d
Dmitry Osipov <dmith.osipov@gmail.com>
2023-12-14 15:38:03 +0400

    remove console.log

--- src/processor.ts
+++ src/processor.ts
@@ -8,7 +8,6 @@ export const indexedNfts = [config.nfts.cb, config.nfts.vit, config.nfts.draft];
 if (config.nfts.old !== '') {
   indexedNfts.push(...config.nfts.old.split(','));
 }
-console.log(config.squid.archive);
 
 export const processor = new SubstrateBatchProcessor()
   .setDataSource({

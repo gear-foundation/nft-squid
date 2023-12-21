c0684e4a5da28042ddb76e3b5f3c86a09c30394e
Dmitry Osipov <dmith.osipov@gmail.com>
2023-12-21 18:44:59 +0400

    fix: mediaurl

--- src/handlers/migrated-nft.ts
+++ src/handlers/migrated-nft.ts
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs';
 
 import config from '../config';
 import { StateReply, StorageStateReply } from '../types';
-import { gearReadStateBatchReq, gearReadStateReq, getDate } from '../utils';
+import { gearReadStateReq, getDate } from '../utils';
 import { BatchState } from '../batchState';
 import { getCollectionDescription, getCollectionName } from './helpers';
 
@@ -26,30 +26,35 @@ export async function readMigratedNfts(state: BatchState) {
   const description = await getCollectionDescription(meta, config.nfts.cb);
   const name = await getCollectionName(meta, config.nfts.cb);
   const collection = state.newCollection(config.nfts.cb, name, description);
-  for (let i = 0; i < storages.length; i += 5) {
-    const result = await gearReadStateBatchReq(storages.slice(i, i + 5), '0x02');
 
-    for (const value of result) {
-      const data = nftMeta.createType<StorageStateReply>((nftMeta.types.state as HumanTypesRepr).output, value);
-      if (data.isAllTokensRawData) {
-        for (const { media, owner, link, activities } of data.asAllTokensRawData) {
-          await state.mintNft(
-            media.toString(),
-            collection,
-            owner.toHex(),
-            activities.map(([name, times, ts]) => {
-              const timesFormatted = name.toString() === 'NFT minted' ? ', ' : ` ${times.toString()} times, `;
-              const date = getDate(ts.toString());
-              const dateFormatted = name.toString() === 'NFT minted' ? `date: ${date}` : `last game date: ${date}`;
-              return `${name}${timesFormatted}${dateFormatted}`;
-            }),
-            description,
-            name + ' - ' + media.toString(),
-            link.toString(),
-            blockNumber,
-            timestamp,
-          );
-        }
+  for (let i = 0; i < storages.length; i++) {
+    console.log('Reading storage', i, 'of', storages.length);
+
+    const result = await gearReadStateReq(storages[i], '0x02');
+
+    const ipfsFolderLink = nftMeta
+      .createType((nftMeta.types.state as HumanTypesRepr).output, await gearReadStateReq(storages[i], '0x06'))
+      .toString();
+
+    const data = nftMeta.createType<StorageStateReply>((nftMeta.types.state as HumanTypesRepr).output, result);
+    if (data.isAllTokensRawData) {
+      for (const { media, owner, activities } of data.asAllTokensRawData) {
+        await state.mintNft(
+          media.toString(),
+          collection,
+          owner.toHex(),
+          activities.map(([name, times, ts]) => {
+            const timesFormatted = name.toString() === 'NFT minted' ? ', ' : ` ${times.toString()} times, `;
+            const date = getDate(ts.toString());
+            const dateFormatted = name.toString() === 'NFT minted' ? `date: ${date}` : `last game date: ${date}`;
+            return `${name}${timesFormatted}${dateFormatted}`;
+          }),
+          description,
+          name + ' - ' + media.toString(),
+          `${ipfsFolderLink}/${media.toString()}`,
+          blockNumber,
+          timestamp,
+        );
       }
     }
   }
